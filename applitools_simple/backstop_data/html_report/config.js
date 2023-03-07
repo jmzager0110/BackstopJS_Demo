0c864534dc447d1c04e6249105b6ea88d3ef09ae
Jessica Zager <jessica.zager@vmlyr.com>
2023-03-07 12:57:49 -0600

    Initial commit

---
+++ applitools_simple/backstop_data/html_report/config.js
@@ -0,0 +1,306 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Demo_Page_0_document_0_phone.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Demo_Page_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Applitools_Demo_Page_0_document_0_phone.png",
+        "label": "Applitools Demo Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 2.586911094224924,
+          "misMatchPercentage": "2.59",
+          "analysisTime": 90
+        },
+        "diffImage": "../bitmaps_test/20230307-125133/failed_diff_backstop_default_Applitools_Demo_Page_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Demo_Page_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Demo_Page_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Applitools_Demo_Page_0_document_1_tablet.png",
+        "label": "Applitools Demo Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.7893880208333334,
+          "misMatchPercentage": "0.79",
+          "analysisTime": 41
+        },
+        "diffImage": "../bitmaps_test/20230307-125133/failed_diff_backstop_default_Applitools_Demo_Page_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Icons_0_logo-w_0_phone.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Icons_0_logo-w_0_phone.png",
+        "selector": ".logo-w",
+        "fileName": "backstop_default_Applitools_Selector_Example-Icons_0_logo-w_0_phone.png",
+        "label": "Applitools Selector Example-Icons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 5.78757225433526,
+          "misMatchPercentage": "5.79",
+          "analysisTime": 21
+        },
+        "diffImage": "../bitmaps_test/20230307-125133/failed_diff_backstop_default_Applitools_Selector_Example-Icons_0_logo-w_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Icons_1_pre-iconos-iconos-icon-user-male-circle_0_phone.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Icons_1_pre-iconos-iconos-icon-user-male-circle_0_phone.png",
+        "selector": ".pre-icon.os-icon.os-icon-user-male-circle",
+        "fileName": "backstop_default_Applitools_Selector_Example-Icons_1_pre-iconos-iconos-icon-user-male-circle_0_phone.png",
+        "label": "Applitools Selector Example-Icons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Icons_2_pre-iconos-iconos-icon-fingerprint_0_phone.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Icons_2_pre-iconos-iconos-icon-fingerprint_0_phone.png",
+        "selector": ".pre-icon.os-icon.os-icon-fingerprint",
+        "fileName": "backstop_default_Applitools_Selector_Example-Icons_2_pre-iconos-iconos-icon-fingerprint_0_phone.png",
+        "label": "Applitools Selector Example-Icons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Icons_0_logo-w_1_tablet.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Icons_0_logo-w_1_tablet.png",
+        "selector": ".logo-w",
+        "fileName": "backstop_default_Applitools_Selector_Example-Icons_0_logo-w_1_tablet.png",
+        "label": "Applitools Selector Example-Icons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.164444444444445,
+          "misMatchPercentage": "3.16",
+          "analysisTime": 16
+        },
+        "diffImage": "../bitmaps_test/20230307-125133/failed_diff_backstop_default_Applitools_Selector_Example-Icons_0_logo-w_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Icons_1_pre-iconos-iconos-icon-user-male-circle_1_tablet.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Icons_1_pre-iconos-iconos-icon-user-male-circle_1_tablet.png",
+        "selector": ".pre-icon.os-icon.os-icon-user-male-circle",
+        "fileName": "backstop_default_Applitools_Selector_Example-Icons_1_pre-iconos-iconos-icon-user-male-circle_1_tablet.png",
+        "label": "Applitools Selector Example-Icons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Icons_2_pre-iconos-iconos-icon-fingerprint_1_tablet.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Icons_2_pre-iconos-iconos-icon-fingerprint_1_tablet.png",
+        "selector": ".pre-icon.os-icon.os-icon-fingerprint",
+        "fileName": "backstop_default_Applitools_Selector_Example-Icons_2_pre-iconos-iconos-icon-fingerprint_1_tablet.png",
+        "label": "Applitools Selector Example-Icons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Inputs_0_form-group_0_phone.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Inputs_0_form-group_0_phone.png",
+        "selector": ".form-group",
+        "fileName": "backstop_default_Applitools_Selector_Example-Inputs_0_form-group_0_phone.png",
+        "label": "Applitools Selector Example-Inputs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Inputs_1_form-group__n1_0_phone.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Inputs_1_form-group__n1_0_phone.png",
+        "selector": ".form-group.__n1",
+        "fileName": "backstop_default_Applitools_Selector_Example-Inputs_1_form-group__n1_0_phone.png",
+        "label": "Applitools Selector Example-Inputs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Inputs_0_form-group_1_tablet.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Inputs_0_form-group_1_tablet.png",
+        "selector": ".form-group",
+        "fileName": "backstop_default_Applitools_Selector_Example-Inputs_0_form-group_1_tablet.png",
+        "label": "Applitools Selector Example-Inputs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Applitools_Selector_Example-Inputs_1_form-group__n1_1_tablet.png",
+        "test": "../bitmaps_test/20230307-125133/backstop_default_Applitools_Selector_Example-Inputs_1_form-group__n1_1_tablet.png",
+        "selector": ".form-group.__n1",
+        "fileName": "backstop_default_Applitools_Selector_Example-Inputs_1_form-group__n1_1_tablet.png",
+        "label": "Applitools Selector Example-Inputs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.applitools.com/index_v2.html",
+        "referenceUrl": "https://demo.applitools.com/index.html#",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
